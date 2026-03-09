3c414b4c999cc788855f24e9668506eac0eaa395
daya112 <24124047@srcas.ac.in>
2026-03-09 15:02:35 +0530

    Add PasswordValidatorTest for password validation
    Added a test for the PasswordValidator class to check password validity.

---
+++ PasswordValidatorTest.java
@@ -0,0 +1,25 @@
+public class PasswordValidatorTest {
+
+    public static void main(String[] args) {
+
+        String password = "TestPass1";
+
+        if (PasswordValidator.isValid(password)) {
+            System.out.println("Password is valid");
+        } else {
+            System.out.println("Password is invalid");
+        }
+    }
+} {
+
+    public static void main(String[] args) {
+
+        String password = "TestPass1";
+
+        if (PasswordValidator.isValid(password)) {
+            System.out.println("Password is valid");
+        } else {
+            System.out.println("Password is invalid");
+        }
+    }
+}
